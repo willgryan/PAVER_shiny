5c87580e52942094af0d85bbbe042c3b162b8bb6
William Ryan <billyryan543@gmail.com>
2024-05-14 01:02:19 -0400

    Basic functionality

--- app.R
+++ app.R
@@ -1,36 +1,52 @@
+library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(PAVER)
-library(ggplot2)
+library(shinyjs)
 
 # UI
 ui <- dashboardPage(
-  dashboardHeader(title = "PAVER Analysis"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Upload Data", tabName = "upload", icon = icon("upload")),
-      menuItem("Settings", tabName = "settings", icon = icon("cogs")),
-      menuItem("Results", tabName = "results", icon = icon("chart-bar"))
-    )
-  ),
+  dashboardHeader(title = "PAVER"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("About", tabName = "about", icon = icon("info-circle")),
+    menuItem("Upload Data", tabName = "upload", icon = icon("upload")),
+    menuItem("Settings", tabName = "settings", icon = icon("cogs")),
+    menuItem("Results", tabName = "results", icon = icon("chart-bar"))
+  )),
   dashboardBody(
+    useShinyjs(),
     tabItems(
-      tabItem(tabName = "upload",
-              h4("You can load example data by clicking below,"),
-              actionButton(label="Load example data", inputId = "loadExample"),
-              h4("Or upload your own CSV file or paste CSV data below."),
-              fileInput("file", "Upload CSV", accept = ".csv"),
-              textAreaInput("csvText", "Or paste CSV data here", rows = 10)
+      tabItem(
+        tabName = "about",
+        h4("About PAVER"),
+        p("PAVER (Pathway Analysis Visualization with Embedding Representations) is a bioinformatics tool to help interpret and visualize pathway analysis results."),
+        p("To use the app, follow these steps:"),
+        tags$ol(
+          tags$li("Navigate to the 'Upload Data' tab to upload your CSV file or paste CSV data directly into the text area."),
+          tags$li("Go to the 'Settings' tab to select your desired pre-computed embedding package and adjust clustering settings."),
+          tags$li("Click the 'Submit' button to start the analysis."),
+          tags$li("View and download the generated plots and clustering results from the 'Results' tab.")
+        )
       ),
-      tabItem(tabName = "settings",
-              selectInput("embeddingPackage", "Choose Embedding Package", choices = c("Package A", "Package B")),
-              sliderInput("paramA", "Clustering Parameter A", min = 1, max = 10, value = 5),
-              sliderInput("paramB", "Clustering Parameter B", min = 1, max = 10, value = 5),
-              actionButton("submit", "Submit")
+      tabItem(
+        tabName = "upload",
+        h4("Upload your own CSV file or paste CSV data below."),
+        fileInput("file", NULL, accept = ".csv"),
+        textAreaInput("csvText", NULL, rows = 10),
+        actionButton(label = "Load example data", inputId = "loadExample")
       ),
-      tabItem(tabName = "results",
-              downloadButton("downloadReport", "Download Report"),
-              uiOutput("plots"),
+      tabItem(
+        tabName = "settings",
+        selectInput("embeddingPackage", "Choose Embedding", choices = c("Gene Ontology", "KEGG")),
+        sliderInput("minClusterSize", "minClusterSize", min = 3, max = 100, value = 3),
+        sliderInput("maxCoreScatter", "maxCoreScatter", min = .1, max = .99, value = .33),
+        actionButton("submit", "Submit")
+      ),
+      tabItem(
+        tabName = "results",
+        downloadButton("downloadReport", "Download Figures"),
+        downloadButton("downloadResults", "Download Results"),
+        uiOutput("plots")
       )
     )
   )
@@ -39,70 +55,94 @@ ui <- dashboardPage(
 # Server
 server <- function(input, output, session) {
   data <- reactive({
-    if (!is.null(input$file)) {
-      read.csv(input$file$datapath, stringsAsFactors = FALSE)
-    } else if (input$csvText != "") {
-      read.csv(text = input$csvText, stringsAsFactors = FALSE)
-    }
-    else if (!is.null(input$loadExample)) {
-      load('./data/gsea_example.rda')
-      gsea_example
-    }
+    if (input$csvText != "") read_csv(input$csvText)
+  })
+  
+  shinyjs::disable("downloadReport")
+  shinyjs::disable("downloadResults")
+  
+  observeEvent(input$loadExample, {
+    example_data <- PAVER::gsea_example %>% filter(across(everything(), ~ . != 0))
+    example_text <- capture.output(write.csv(example_data, row.names = FALSE, quote = FALSE))
+    updateTextAreaInput(session, "csvText", value = paste(example_text, collapse = "\n"))
+  })
+  
+  observeEvent(input$file, {
+    req(input$file)
+    file_data <- read_csv(input$file$datapath)
+    file_text <- capture.output(write.csv(file_data, row.names = FALSE, quote = FALSE))
+    updateTextAreaInput(session, "csvText", value = paste(file_text, collapse = "\n"))
   })
   
   analysis <- eventReactive(input$submit, {
     req(data())
-    # Placeholder for PAVER analysis function
-    # Replace this with the actual PAVER analysis call
-    # paverResult <- paver(data(), embedding_package = input$embeddingPackage, paramA = input$paramA, paramB = input$paramB)
+    shinyjs::disable("submit")
+    shinyjs::disable("downloadReport")
+    shinyjs::disable("downloadResults")
+    shinyjs::hide("plots")
     
-    # For demonstration, creating dummy plots
-    list(
-      plot1 = ggplot(mtcars, aes(wt, mpg)) + geom_point(),
-      plot2 = ggplot(mtcars, aes(wt, qsec)) + geom_point(),
-      plot3 = ggplot(mtcars, aes(wt, hp)) + geom_point(),
-      plot4 = ggplot(mtcars, aes(wt, drat)) + geom_point()
-    )
+    withProgress(message = 'Grabbing embeddings...', value = 0, {
+      embeddings_url <- "https://github.com/willgryan/PAVER_embeddings/raw/main/2023-03-06/embeddings_2023-03-06.RDS"
+      term2name_url <- "https://github.com/willgryan/PAVER_embeddings/raw/main/2023-03-06/term2name_2023-03-06.RDS"
+      embeddings <- readRDS(url(embeddings_url))
+      term2name <- readRDS(url(term2name_url))
+      
+      incProgress(0.2, detail = "Preparing data...")
+      PAVER_result <- PAVER::prepare_data(data(), embeddings, term2name)
+      
+      incProgress(0.4, detail = "Generating themes...")
+      minGap <- (1 - input$maxCoreScatter) * 3 / 4
+      PAVER_result <- PAVER::generate_themes(PAVER_result, minClusterSize = input$minClusterSize, maxCoreScatter = input$maxCoreScatter, minGap = minGap)
+      
+      incProgress(0.6, detail = "Creating plots...")
+      plots <- list(
+        plot1 = PAVER_theme_plot(PAVER_result),
+        plot2 = PAVER_interpretation_plot(PAVER_result),
+        plot3 = PAVER_regulation_plot(PAVER_result),
+        plot4 = PAVER_hunter_plot(PAVER_result)
+      )
+      
+      incProgress(0.8, detail = "Exporting results...")
+      clustered_input <- PAVER_export(PAVER_result)
+      
+      incProgress(1, detail = "Done")
+      shinyjs::enable("downloadReport")
+      shinyjs::enable("downloadResults")
+      shinyjs::enable("submit")
+      shinyjs::reset("csvText")
+      shinyjs::reset("file")
+      shinyjs::show("plots")
+      
+      list(plots = plots, clustered_input = clustered_input)
+    })
   })
   
   output$plots <- renderUI({
     req(analysis())
-    plot_output_list <- lapply(1:4, function(i) {
-      plotname <- paste("plot", i, sep="")
-      plotOutput(plotname, height = 300)
-    })
+    plot_output_list <- lapply(1:4, function(i) plotOutput(paste0("plot", i), height = 300))
     do.call(tagList, plot_output_list)
   })
-  output$plot1 <- renderPlot({
-    req(analysis())
-    analysis()$plot1
-  })
-  output$plot2 <- renderPlot({
-    req(analysis())
-    analysis()$plot2
-  })
-  output$plot3 <- renderPlot({
-    req(analysis())
-    analysis()$plot3
-  })
-  output$plot4 <- renderPlot({
-    req(analysis())
-    analysis()$plot4
+  
+  lapply(1:4, function(i) {
+    output[[paste0("plot", i)]] <- renderPlot({
+      req(analysis())
+      analysis()$plots[[paste0("plot", i)]]
+    })
   })
   
   output$downloadReport <- downloadHandler(
-    filename = function() {
-      paste("PAVER_analysis_report", Sys.Date(), ".pdf", sep="")
-    },
+    filename = function() paste0("PAVER_analysis_report_", Sys.Date(), ".pdf"),
     content = function(file) {
       pdf(file)
-      for (i in 1:4) {
-        print(analysis()[[paste("plot", i, sep="")]])
-      }
+      lapply(1:4, function(i) print(analysis()$plots[[paste0("plot", i)]]))
       dev.off()
     }
   )
+  
+  output$downloadResults <- downloadHandler(
+    filename = function() paste0("PAVER_clustering_results_", Sys.Date(), ".csv"),
+    content = function(file) write_csv(analysis()$clustered_input, file)
+  )
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
